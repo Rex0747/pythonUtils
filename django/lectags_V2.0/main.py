a79d48d25c4f6bb129ba23809bce8675bee5d01d
rex0747 <peli0747@gmail.com>
2021-04-18 22:34:13 +0200

    18042021

--- django/lectags_V2.0/main.py
+++ django/lectags_V2.0/main.py
@@ -1,32 +1,57 @@
-#! /home/pi/Python/lectags/env/bin/python3.7
+#!/usr/local/bin/python3.7
 # -*- coding: utf-8 -*-
+import os
 import qr
-#import RPi.GPIO as GPIO
-#GPIO.setmode(GPIO.BCM)
-# 
-#GPIO.setup(22, OUT)
-#GPIO.setup(27, OUT)
+from gpiozero import LED
+from time import sleep
 import time
 import datetime
 import platform
 import keyboard
 from sqlite.sqlite import baseData
 
-time.sleep(1)
+led_green = LED(17)
+led_red = LED(23)
 
-sistema = platform.system()
+#import RPi.GPIO as GPIO
+#GPIO.setmode(GPIO.BCM)
+# 
+#GPIO.setup(22, OUT)
+#GPIO.setup(27, OUT)
 
+def opposite(device):
+    for value in device.values:
+        yield not value
 
+def change(device, tiempo):    #Cambiar estado de pin
+    if device.is_active:
+        device.off()
+    else:
+        device.on()
+    device.source = opposite(device)
+    #print('Led Encendido: ', device.active_high, ' Activo: ', device.is_active)
+    sleep(tiempo)
 
+def ledOn(device):
+    device.on()
 
-txt = ''
-lista = []
-delim = '*'
-borrar = '1551'
-comprobar = '021992'
-enviar = '010304'
+def ledOff(device):
+    device.off()
 
+def ledOnTime(device, tiempo):
+    device.on()
+    print('Led encendido.')
+    sleep(tiempo)
+    device.off()
+    print('Led apagado.')
 
+def ledRafaga(device,npulsos,tiempo):
+    for i in range(npulsos):
+        change(device,0)
+        print('Estado: ',device.is_active)
+        sleep(tiempo)
+
+    
 def inicio(data):
     #print(data)
     if data == enviar:
@@ -36,17 +61,20 @@ def inicio(data):
     elif data == borrar:
         borrarPedido()
     else:
-        db = baseData('/home/pi/Python/lectags/db.s3db' )
+        db = baseData(ruta_db )
         res = db.compararTags(data)
         if res == False:
             print('Insertando: ', str(data))
             db.insertarDato( data )
             #poner aqui chivato de luz verde de ok
+            change(led_green)
         else:
             print('Error... Referencia duplicada, no se incluye en pedido.')   # Poner aqui chivato luz roja de error.
             print('DATO: ', data)
+            ledOnTime(led_red, 3)
+
 def enviarPedido():
-    db = baseData('/home/pi/Python/lectags/db.s3db' )
+    db = baseData(ruta_db )
     cuantos = db.compEnvioNoVacio()
     print('Nfilas: ', str(cuantos))
     if cuantos > 0:
@@ -55,11 +83,11 @@ def enviarPedido():
         print('No se puede enviar un pedido vacio')
     
 def revisarPedido():
-    db = baseData('/home/pi/Python/lectags/db.s3db' )
+    db = baseData( ruta_db )
     db.revPedido()
     
 def borrarPedido():
-    db = baseData('/home/pi/Python/lectags/db.s3db' )
+    db = baseData(ruta_db )
     db.borrarPedido()
 
 def comprobarFormato():
@@ -69,11 +97,43 @@ def comprobarFormato():
     else:
         return False
 
+#_______Declaraciones____________
+
+
+
+# led.on()
+# led.source = opposite(led)
+# print('Led Encendido: ', led.active_high, ' Activo: ', led.is_active)
+# sleep(2)
+# led.off()
+# led.source = opposite(led)
+# print('Led Encendido: ', led.active_high, ' Activo: ', led.is_active)
+# sleep(2)
+
+time.sleep(1)
+
+sistema = platform.system()
+
+txt = ''
+lista = []
+delim = '*'
+borrar = '1551'
+comprobar = '021992'
+enviar = '010304'
+
+ledRafaga(led_green,9,0.2)
+sleep(2)
+ledOnTime(led_red, 4)
+
+#______End Declaraciones__________
 
 if(__name__ == '__main__'):
     txt = ''
     v = ''
+    ruta = os.getcwd()
+    ruta_db = ruta + '/db.s3db'
     print('OS: ', sistema)
+    print('Directotio actual: ', ruta)
     #while v != '028187':
     while(True):
         events = keyboard.record('enter')
@@ -82,11 +142,12 @@ if(__name__ == '__main__'):
         #keyboard.play(events)
         v = list(txt)[0]
         data = v.replace('+','*')
-        
-        fila = open('/home/pi/Python/lectags/lg.log','a+')
+        fila = open( ruta + '/lg.log','a+')
         fila.write('Linea: ' + data +'\n')
         fila.close()
-        
         inicio(data)
-        #mtx.append(v)
+
+
+
+
 
---
+++ django/lectags_V2.0/qr.py
@@ -0,0 +1,26 @@
+import pyqrcode
+
+
+class qr:
+
+
+    def __init__(self):
+
+        #self.articulo = input('Lee lector qr')
+        #self.tmp = pyqrcode.QRCode( self.articulo, error='H', version=3, mode=None, encoding='iso-8859-1')
+            #tmp = pyqrcode.create(articulo)
+            #print(tmp.text())
+
+            #enviar = input('Enviar linea S/N ' + articulo)
+            #if(enviar == 'S' or enviar == 's'):
+
+            #print('Codigo enviado')
+            #self.tmp.show()
+        #self.lista =self.articulo.split('|')
+        pass
+    
+    def getVal(self):
+        #return self.lista
+        pass
+
+    
